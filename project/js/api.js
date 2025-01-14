648d8ef31c6c5bfb3e6f8def704049a79d5d5512
hyerin-kang <hkang0420@gmail.com>
2025-01-14 16:43:49 +0900

    모달 구현

--- project/js/api.js
+++ project/js/api.js
@@ -1,5 +1,7 @@
 //공통ui(카드들)
+
 function renderMovies(movies) {
+  console.log(movies);
   let movieList = document.getElementById("movie-list");
   movieList.innerHTML = "";
 
@@ -11,9 +13,7 @@ function renderMovies(movies) {
     let movieImg = movie.poster_path;
 
     //ui생성
-
     const liId = movie.id;
-
     let tempLi = document.createElement("li");
     tempLi.id = liId;
     // <img src="${imageUrl}" alt="${movieTitle}" />
@@ -84,7 +84,7 @@ const options = {
   headers: {
     accept: "application/json",
     Authorization:
-      "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI5ZjVlYjIyMWUwNDAwOWJmNDAwMTQ0YTFiZmRkMjFiNyIsIm5iZiI6MTcyMTYzMTE3Ny43MzcsInN1YiI6IjY2OWUwMWM5YzVlMjBkNjJhODE4NzI1NSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.ym4S7Leagjj66RX7Fyp72RQmMTi-JO7sUuVueyKcCKM",
+      "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI1ODk4ZWI2NzgyN2U3NzI5N2FlY2QzNzI3ODBiZmNmMyIsIm5iZiI6MTcyNzE0MjAxMi4yMDk5OTk4LCJzdWIiOiI2NmYyMTg3YzczMDBhNWJhMjEzYmNlMWIiLCJzY29wZXMiOlsiYXBpX3JlYWQiXSwidmVyc2lvbiI6MX0.Cd-6Hd3HasTqkOAmz02TRipcr9tUyeqW8cVNt8HuOAw",
   },
 };
 
@@ -102,7 +102,7 @@ fetch(
 //검색기능
 const input = document.querySelector("#searchInput");
 const inputForm = document.querySelector("#searchForm");
-input.addEventListener("input", function (e) {
+input.addEventListener("keyup", function (e) {
   const inputVal = e.target.value.trim().toLowerCase();
   if (!inputVal) {
     fetch(
@@ -116,32 +116,51 @@ input.addEventListener("input", function (e) {
       .catch((err) => console.error(err));
   } else {
     fetch(
-      "https://api.themoviedb.org/3/search/movie?query=%EC%8A%88%ED%8D%BC&include_adult=false&language=ko-KR&page=1",
+      `https://api.themoviedb.org/3/search/movie?query=${inputVal}&include_adult=false&language=ko-KR&page=1`,
       options
     )
       .then((res) => res.json())
 
       .then((data) => {
-        // renderMovies(data.results);
-        console.log(data.results, "검색");
-
-        // renderMovies(res);
-        //제목 배열로 바꾸기기
-        const inputVal = e.target.value.toLowerCase();
-        const includeVal = data.results.filter((movie) => {
-          return movie.title.toLowerCase().includes(inputVal);
-        });
-        if (includeVal.length == 0) {
-          console.log(includeVal, " 없음");
+        if (data.results.length > 0) {
+          renderMovies(data.results);
+        } else {
           const movieList = document.getElementById("movie-list");
           movieList.innerHTML = `<p>검색결과가 없습니다</p>`;
-        } else {
-          renderMovies(includeVal); // ui 생성
         }
+
+        // ui 생성
+        // renderMovies(data.results);
+        // console.log(data.results, "검색");
+
+        //제목 배열로 바꾸기기
+        // const inputVal = e.target.value.toLowerCase();
+        // const includeVal = data.results.filter((movie) => {
+        //   return movie.title.toLowerCase().includes(inputVal);
+        // });
+        // if (includeVal.length == 0) {
+        //   // console.log(includeVal, " 없음");
+        //   const movieList = document.getElementById("movie-list");
+        //   movieList.innerHTML = `<p>검색결과가 없습니다</p>`;
+        // } else {
+
+        // }
       })
       .catch((err) => console.error(err));
   }
 });
+
+//모달 열기기
+// document.getElementById("movie-list").addEventListener("click", function (e) {
+//   const clickedCard = e.target.closest("li"); // 클릭한 영화 카드(li) 찾기
+//   if (clickedCard) {
+//     const movieId = clickedCard.id; // 클릭한 카드의 ID
+//     const movie = moviesArr.find((m) => m.id == movieId); // 해당 ID에 해당하는 영화 찾기
+//     if (movie) {
+//       openModal(movie); // 해당 영화의 모달을 열기
+//     }
+//   }
+// });
 
 /*
   공통ui 빼기 (데이터 받아와서)
@@ -166,3 +185,7 @@ input.addEventListener("input", function (e) {
       1. search
 
   */
+//로컬스토리지
+// window.localStorage.setItem(key, value);
+// window.localStorage.getItem(key);
+//movies 형태 [{id : 111, original_title : '제목'}, {}, {}]
