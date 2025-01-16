d4994634bff8787af910ac210c1216c8eeda1af3
hyerin-kang <hkang0420@gmail.com>
2025-01-16 16:41:16 +0900

    북마크기능

--- project/js/api.js
+++ project/js/api.js
@@ -1,23 +1,32 @@
 //공통ui(카드들)
-
-function renderMovies(movies) {
-  console.log(movies);
+function renderMovies(movies_1) {
+  //movies_1 : 영화 데이터의 배열들 (data.results로 전달했었음)
+  // console.log(movies);
   let movieList = document.getElementById("movie-list");
   movieList.innerHTML = "";
+  movies_1.forEach((movie) => {
+    const tempLi = createMovieCard(movie); // li태그하나
+    movieList.appendChild(tempLi);
+    //카드 클릭시 모달 열기
+    tempLi.addEventListener("click", () => {
+      openModal(movie);
+    });
+  });
+}
 
-  movies.forEach((movie) => {
-    let movieTitle = movie.title;
-    // let movieOverview = movie.overview;
-    let movieAvg = movie.vote_average;
-    let imgURl = "https://image.tmdb.org/t/p/w500/";
-    let movieImg = movie.poster_path;
+function createMovieCard(movie1) {
+  let movieTitle = movie1.title;
+  // let movieOverview = movie.overview;
+  let movieAvg = movie1.vote_average;
+  let imgURl = "https://image.tmdb.org/t/p/w500/";
+  let movieImg = movie1.poster_path;
 
-    //ui생성
-    const liId = movie.id;
-    let tempLi = document.createElement("li");
-    tempLi.id = liId;
-    // <img src="${imageUrl}" alt="${movieTitle}" />
-    tempLi.innerHTML = `
+  //ui생성
+  const liId = movie1.id;
+  let tempLi = document.createElement("li");
+  tempLi.id = liId;
+  // <img src="${imageUrl}" alt="${movieTitle}" />
+  tempLi.innerHTML = `
           <div class="img">
             <img src="${imgURl}${movieImg}" alt="${movieTitle}" />
           </div>
@@ -26,40 +35,33 @@ function renderMovies(movies) {
             <p>평점 : ${movieAvg}</p>
           </div>
       `;
-    movieList.appendChild(tempLi);
 
-    //카드 클릭시 모달 열기
-    tempLi.addEventListener("click", () => {
-      console.log("모달 클릭");
-      openModal(movie);
-    });
-  });
+  return tempLi;
 }
 
 //모달 함수
-function openModal(movie) {
+function openModal(movie_2) {
   const modal = document.getElementById("modal");
   modal.style.display = "block";
   modal.innerHTML = "";
-  console.log("열림");
 
   const imgURl = "https://image.tmdb.org/t/p/w500/";
-  const movieImg = movie.poster_path;
+  const movieImg = movie_2.poster_path;
   const modalTemp = document.createElement("div");
-  modalTemp.id = movie.id;
+  modalTemp.id = movie_2.id;
   modalTemp.innerHTML = `
       <div class="modal-content">
         <div class="title">
-          <h1>${movie.title}</h1>
+          <h1>${movie_2.title}</h1>
           <button class="close">닫기</button>
         </div>
         <div class="content">
           <div class="img">
-            <img src="${imgURl}${movieImg}" alt="${movie.title}" />
+            <img src="${imgURl}${movieImg}" alt="${movie_2.title}" />
           </div>
           <div class="txt">
-            <p>${movie.overview}</p>
-            <button class='bookMark'>북마크 저장하기</button>
+            <p>${movie_2.overview}</p>
+            <button class='bookMarBtn'>북마크 저장하기</button>
           </div>
         </div>
       </div>
@@ -68,17 +70,61 @@ function openModal(movie) {
 
   const closeBtn = document.querySelector(".close");
   closeBtn.addEventListener("click", function (e) {
-    // alert(e.target);
     modal.style.display = "none";
-    //새롭게 패치 하는 로직이 필요하다.
-    //
+  });
+  //북마크 저장
+  document.querySelector(".bookMarBtn").addEventListener("click", function (e) {
+    alert("북마크 저장");
+
+    const localStorageBookMarkList = localStorage.getItem("bookMarkList"); //bookMarkList 키값으로 저장된 value 값들을 가져와
+    // localStorage.setItem("bookMarkList", JSON.stringify(movie)); //movie값을 bookMarkList키 값으로 저장해라
+
+    let bookMarkList; // 일단 선언해두기
+    // bookMarkList.push(movie); // movie를 bookMarkList에 추가
+    if (!localStorageBookMarkList) {
+      //저장해둔게 아무거도 없으면
+      bookMarkList = []; //빈값으로
+    } else {
+      // 저장해둔게 있으면 아까 문자열로 바꾼것들을 객체로 변환하여 bookMarkList 에 할당한다.
+      bookMarkList = JSON.parse(localStorageBookMarkList);
+      // console.log(bookMarkList);
+    }
+
+    //중복된거  추가안되게 짜라
+    console.log(movie_2, "선택한거");
+
+    bookMarkList.push(movie_2);
+    localStorage.setItem("bookMarkList", JSON.stringify(bookMarkList));
   });
 }
 
-//모달 ui생성성
-//카드ui에 있는거랑같은정보가 보이게
+//북마크 된 목록 보여주기
+let isBookMark = false; //처음 보여지는 화면이 인기영화이니까 북마크된거를 false로 초기설정
+function showBookmarkAndPopular(movies_1) {
+  // console.log("저장된 북마크들 보여주기");
+  const localStorageBookMarkList = localStorage.getItem("bookMarkList");
+  const bookMarkBtn = document.querySelector(".bookMark");
 
-//인기영화 불러오기
+  if (isBookMark) {
+    // 인기 영화가 보일때
+    bookMarkBtn.innerHTML = "북마크 보기";
+    fetchPopularMovies(options);
+  } else {
+    //찜한 목록 보여주기
+    bookMarkBtn.innerHTML = "인기영화 목록";
+    if (!localStorageBookMarkList) {
+      document.getElementById("movie-list").innerHTML =
+        "<p>북마크된 영화가 없습니다 :(</p>";
+    } else {
+      renderMovies(JSON.parse(localStorageBookMarkList)); //찜한영화 보여주기
+    }
+  }
+  isBookMark = !isBookMark; //클릭할때마다 true, false로 바꾸기 (토글)
+}
+document.querySelector(".bookMark").addEventListener("click", function () {
+  showBookmarkAndPopular();
+});
+
 const options = {
   method: "GET",
   headers: {
@@ -87,17 +133,20 @@ const options = {
       "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI1ODk4ZWI2NzgyN2U3NzI5N2FlY2QzNzI3ODBiZmNmMyIsIm5iZiI6MTcyNzE0MjAxMi4yMDk5OTk4LCJzdWIiOiI2NmYyMTg3YzczMDBhNWJhMjEzYmNlMWIiLCJzY29wZXMiOlsiYXBpX3JlYWQiXSwidmVyc2lvbiI6MX0.Cd-6Hd3HasTqkOAmz02TRipcr9tUyeqW8cVNt8HuOAw",
   },
 };
-
-fetch(
-  "https://api.themoviedb.org/3/movie/popular?language=ko-KR&page=1",
-  options
-)
-  .then((res) => res.json())
-  .then((data) => {
-    //데이터가 받아와졌을때 실행되는 코드들
-    renderMovies(data.results); // 데이터 받아서 ui 생성
-  })
-  .catch((err) => console.error(err));
+//인기영화 불러오기
+function fetchPopularMovies(options) {
+  fetch(
+    "https://api.themoviedb.org/3/movie/popular?language=ko-KR&page=1",
+    options
+  )
+    .then((res) => res.json())
+    .then((data) => {
+      //데이터가 받아와졌을때 실행되는 코드들
+      renderMovies(data.results); // 데이터 받아서 ui 생성
+    })
+    .catch((err) => console.error(err));
+}
+fetchPopularMovies(options);
 
 //검색기능
 const input = document.querySelector("#searchInput");
@@ -105,22 +154,13 @@ const inputForm = document.querySelector("#searchForm");
 input.addEventListener("keyup", function (e) {
   const inputVal = e.target.value.trim().toLowerCase();
   if (!inputVal) {
-    fetch(
-      "https://api.themoviedb.org/3/movie/popular?language=ko-KR&page=1",
-      options
-    )
-      .then((res) => res.json())
-      .then((data) => {
-        renderMovies(data.results); // 인기 영화 목록 다시 렌더링
-      })
-      .catch((err) => console.error(err));
+    fetchPopularMovies();
   } else {
     fetch(
       `https://api.themoviedb.org/3/search/movie?query=${inputVal}&include_adult=false&language=ko-KR&page=1`,
       options
     )
       .then((res) => res.json())
-
       .then((data) => {
         if (data.results.length > 0) {
           renderMovies(data.results);
@@ -128,64 +168,7 @@ input.addEventListener("keyup", function (e) {
           const movieList = document.getElementById("movie-list");
           movieList.innerHTML = `<p>검색결과가 없습니다</p>`;
         }
-
-        // ui 생성
-        // renderMovies(data.results);
-        // console.log(data.results, "검색");
-
-        //제목 배열로 바꾸기기
-        // const inputVal = e.target.value.toLowerCase();
-        // const includeVal = data.results.filter((movie) => {
-        //   return movie.title.toLowerCase().includes(inputVal);
-        // });
-        // if (includeVal.length == 0) {
-        //   // console.log(includeVal, " 없음");
-        //   const movieList = document.getElementById("movie-list");
-        //   movieList.innerHTML = `<p>검색결과가 없습니다</p>`;
-        // } else {
-
-        // }
       })
       .catch((err) => console.error(err));
   }
 });
-
-//모달 열기기
-// document.getElementById("movie-list").addEventListener("click", function (e) {
-//   const clickedCard = e.target.closest("li"); // 클릭한 영화 카드(li) 찾기
-//   if (clickedCard) {
-//     const movieId = clickedCard.id; // 클릭한 카드의 ID
-//     const movie = moviesArr.find((m) => m.id == movieId); // 해당 ID에 해당하는 영화 찾기
-//     if (movie) {
-//       openModal(movie); // 해당 영화의 모달을 열기
-//     }
-//   }
-// });
-
-/*
-  공통ui 빼기 (데이터 받아와서)
-
-  검색기능:
-  인풋창이 입력받고 있는 이벤트리스터 안에
-  search api를 요청하고
-  fetch해서 가져온 데이터를 공통 ui함수에 넣어준다.
-
-        // console.log(searchTitle);
-      //배열에서 검색한거랑 같은거 걸러줘
-      // const inputVal = e.target.value;
-      // const filteredSearch = searchTitle.filter(function (title) {
-      //   return title.toLowerCase().includes(inputVal.toLowerCase());
-      // });
-
-      // console.log(inputVal, " : 입력값");
-      // console.log(filteredSearch, " : 검색 제목");
-      // console.log(res, "검색데이터");
-      // renderMovies(filteredSearch);
-      /*
-      1. search
-
-  */
-//로컬스토리지
-// window.localStorage.setItem(key, value);
-// window.localStorage.getItem(key);
-//movies 형태 [{id : 111, original_title : '제목'}, {}, {}]
